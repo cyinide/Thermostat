3dd024e03366c80d05c4fa69e0f5966a9102ea5e
Damir Krkalić <damirkrkalic11@gmail.com>
2021-04-20 21:59:08 +0200

    custom gauge control - implementation and adjustments

--- Thermostat/ViewModels/Base/BaseViewModel.cs
+++ Thermostat/ViewModels/Base/BaseViewModel.cs
@@ -1,10 +1,28 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace Thermostat.ViewModels.Base
 {
     public class BaseViewModel : ExtendedBindableObject
     {
+
+        public virtual async Task OnAppearing()
+        {
+            var output = string.Format("ViewModel: {0} is appearing");
+            Debug.WriteLine(output);
+
+            await Task.FromResult(true);
+        }
+        public virtual async Task OnDisappearing()
+        {
+            var output = string.Format("ViewModel: {0} is disappearing");
+            Debug.WriteLine(output);
+
+            await Task.FromResult(true);
+        }
+
     }
 }
--- Thermostat/ViewModels/ThermostatViewModel.cs
+++ Thermostat/ViewModels/ThermostatViewModel.cs
@@ -7,5 +7,22 @@ namespace Thermostat.ViewModels
 {
     public class ThermostatViewModel : BaseViewModel
     {
+        public ThermostatViewModel()
+        {
+
+        }
+        public string _stringtest = "teststring";
+        public string StringTest
+        {
+            get
+            {
+                return _stringtest;
+            }
+            set
+            {
+                _stringtest = value;
+                RaisePropertyChanged(() => StringTest);
+            }
+        }
     }
 }
